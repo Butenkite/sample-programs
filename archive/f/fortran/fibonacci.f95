a1dd9c170cbb6bb26c3cca524c3b07d96119fd12
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-02-11 13:16:42 -0800

    Psuedo code added

---
+++ archive/f/fortran/fibonacci.f95
@@ -0,0 +1,17 @@
+! In program name, - is not allowed
+program fibonacci
+
+! Define Previous number (0?)    
+! Define Current number (1?)     
+! Define an Added number (0?)
+! Define a counter (0?)
+! Define and Recieve a number of loops from the user
+
+
+! Loop if number of loops is greater than 0, and is greater than counter
+! Increase counter by one
+! Set Added Number to Previous Number + Current Number
+! Set Previous Number to Current Number
+! Set Current Number to Added Number
+! Say "Counter: Added Number"
+! Repeat or End loop
