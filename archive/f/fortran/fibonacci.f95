34f3440f9924b386e8c6ed9f72ef37a2cc437e63
Niko_Butenko <174657090+Butenkite@users.noreply.github.com>
2025-02-13 12:42:34 -0800

    Implemented basic fibonacci iteration. Does not pass tests.

--- archive/f/fortran/fibonacci.f95
+++ archive/f/fortran/fibonacci.f95
@@ -1,26 +1,24 @@
 ! In program name, - is not allowed
 program fibonacci
 
-
-integer :: previousnumber, currentnumber, addednumber, counter, reciever
+! Create the variables
+integer :: reciever
+integer(kind = 16) :: previousnumber, currentnumber, addednumber
 character(len = 60) :: message
+
+! Define the variables.
+previousnumber = 0
+currentnumber = 1
+addednumber = 0
 message = "Usage: please input the count of fibonacci numbers to output"
-print *, message
-read *, reciever
-print *, reciever
-! Define Previous number (0?)    
-! Define Current number (1?)     
-! Define an Added number (0?)
-! Define a counter (0?)
-! Define and Recieve a number of loops from the user
+print *, message   ! Request from user
+read *, reciever   ! Input from user      
 
-
-! Loop if number of loops is greater than 0, and is greater than counter
-! Increase counter by one
-! Set Added Number to Previous Number + Current Number
-! Set Previous Number to Current Number
-! Set Current Number to Added Number
-! Say "Counter: Added Number"
-! Repeat or End loop
-
+! Enter a loop
+do i = 1, reciever
+    print *, i, ": ", currentnumber                 ! Print current iteration, and current number
+    addednumber = previousnumber + currentnumber    ! Replace the value of 'added number' with both 'current number' and 'previous number'
+    previousnumber = currentnumber                  ! Replace the value of 'previous number' with 'current number'
+    currentnumber = addednumber                     ! Replace the value of 'current number' with 'added number'
+end do
 end program fibonacci
